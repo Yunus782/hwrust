12ad6c44e33addae12e1c91f5662e6cb37568a50
asdlv <yunus.asadulaev@mail.ru>
2022-12-02 00:28:09 +0300

    f commit

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ fn main() {
     let mut tempf: std::fs::File = std::fs::File::open(path).expect("Error: file not find");
     let mut data = String::new();
     tempf.read_to_string(&mut data).expect("Error");
-    let jsonfile: serde_json::Value = serde_yaml::from_str(&data).unwrap();
+    let jsonfile: serde_json::Value = toml::from_str(&data).unwrap();
     println!("{}", serde_json::to_string_pretty(&jsonfile).unwrap());
 }
